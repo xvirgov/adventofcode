85d9aa298d6b8a034f8ab68e9b9cbdc4b41f38dc
Michal Virgovic <michal.virgovic-ext@hexagon.com>
2022-12-10 21:15:47 +0100

    restructure

---
+++ 2022/day-1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("aaa")
+
+	lines := LinesInFile("2022/day-10.txt")
+	fmt.Println(lines)
+}
---
+++ common/io.go
@@ -0,0 +1,20 @@
+package common
+
+import (
+	"bufio"
+	"os"
+)
+
+func LinesInFile(fileName string) []string {
+	f, _ := os.Open(fileName)
+	// Create new Scanner.
+	scanner := bufio.NewScanner(f)
+	result := []string{}
+	// Use Scan.
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Append line to result.
+		result = append(result, line)
+	}
+	return result
+}
